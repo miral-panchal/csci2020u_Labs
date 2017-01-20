ae9a986f66d5434def14b4983cc33263652c2265
Miral <miral.panchal@uoit.net>
2017-01-20 18:33:29 -0500

    Finished Lab 02

--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -1,5 +1,6 @@
 public class SimpleTest {
-	public static void main(String args[]) {
-		System.out.println("Hello World");	
+	public static void main (String args[]) {
+		System.out.println("Hello World");
 	}
+
 }
